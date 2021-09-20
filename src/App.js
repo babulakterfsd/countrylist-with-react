0699eec23f897f7bb0666939dcc08473f7e6a6a1
babulakterfsd <babulakterfsd@gmail.com>
2021-09-20 13:06:31 +0600

    animation added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'animate.css';
 import { useEffect, useState } from 'react';
 import './App.css';
 
@@ -27,7 +28,7 @@ function GetAndDisplayCountries(props) {
       .then(data => setCountries(data.slice(0,20)))
   }, [])
   return (
-    <div className="countryContainer">
+    <div className="countryContainer py-5 animate__animated animate__zoomIn">
      {countries.map(country => <div className="card"><img class="card-img-top" src={country.flag} alt="country" style={{height: '100px', objectFit: 'cover'}}></img><div className="card-body"><div className="card-title">country : <span className="text-success fw-bolder">{country.name}</span></div><div className="card-text"><p>capital : <span className="fw-bold text-success">{country.capital}</span></p><p>population : <span className="fw-bold text-success">{country.population}</span></p><p>region : <span className="fw-bold text-success">{country.region}</span></p></div></div></div>)}
     </div>
   )
